9ea04a8af34132093809ef961a7aaa08b48f4db4
mattwong59 <mattwong59@gmail.com>
2018-04-05 03:19:19 +0000

    Added dummie json that appends to poll-container

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,5 +6,38 @@ $(() => {
     for(user of users) {
       $("<div>").text(user.name).appendTo($("body"));
     }
-  });;
+  });
+
+
+
+  $('.createPoll').on('click',function(poll) {
+  $('.poll-container').append(poll.title)
+    options.forEach(function(option) {
+      $('.poll-container').append("<p>" + option.title + "</p>");
+      $('.poll-container').append("<p>" + option.description + "</p>");
+    })
+  });
 });
+
+const poll = {
+
+    "title": "What food?",
+    "email": "bob@bob.bob",
+    "options": [
+       { "title": "Cheeseburger",
+        "description": "Hot and cheesy",
+        "rank": 0
+        },
+        { "title": "Sushi",
+          "description": "Fresh and delicious",
+          "rank": 0
+        },
+        { "title": "Pasta",
+          "description": "Yummy",
+          "rank": 0
+      }]
+    };
+
+let options = poll.options;
+
+
