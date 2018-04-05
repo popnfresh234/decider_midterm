9a7d8fbf66832205c1361fb5f8028152ccf6c8fe
Alex Holliday <ajhollid@gmail.com>
2018-04-05 01:02:21 +0000

    added basic poll routes with logging confirmations

---
+++ routes/polls.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (knex) => {
+
+  router.get("/", (req, res) => {
+    console.log("GET /");
+    res.send("Succesful GET /");
+  });
+
+  router.get("/polls/:id", (req, res)=> {
+    let id = req.params.id;
+    console.log("/polls/:id: ", id);
+    res.send("Succesful GET /:id with id: " + id);
+  });
+
+  router.get("/polls/:id/result", (req, res)=> {
+    let id = req.params.id;
+    console.log("/polls/:id/result: ", id);
+    res.send("Succesful GET /polls/:id with id: " + id);
+  });
+
+  router.post("/polls/", (req, res) => {
+    let poll = req.body.poll;
+    console.log("POST / ", poll);
+    res.send("Succesful POST /polls with " + poll);
+  });
+
+  router.put("/polls/:id", (req, res) => {
+    let id = req.params.id;
+    res.send("Succesful PUT /polls/:id with id: " + id);
+  });
+  return router;
+};
