532891edfb0639f836b9103a086e1a4d767e17eb
Andy <voandy1998@gmail.com>
2018-04-06 00:41:32 +0000

    fixed conflicting name for poll title and option title

--- db/seeds/dummy.js
+++ db/seeds/dummy.js
@@ -4,8 +4,8 @@ exports.seed = function(knex, Promise) {
   return knex('poll')
     .then(function () {
       return Promise.all([
-        knex('poll').insert({title: 'What Movie?', email: 'email@email.com'}),
-        knex('poll').insert({title: 'What Food?', email: 'food@food.com'}),
+        knex('poll').insert({ptitle: 'What Movie?', email: 'email@email.com'}),
+        knex('poll').insert({ptitle: 'What Food?', email: 'food@food.com'}),
       ]);
     });
 };
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -88,4 +88,3 @@ const poll = {
 
 let options = poll.options;
 
-
