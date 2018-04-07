88df4f9dbb2fffac9e764d9afb97f543ebf1b496
mattwong59 <mattwong59@gmail.com>
2018-04-07 17:39:08 +0000

    Added more styling

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -76,7 +76,7 @@ $(() => {
       if (optionTitle === '') {
         $.flash('Please enter an option title.')
       }else {
-      $('.poll-options').append('<li class="optionItem"><p class="optionTitle">' + $('.option').val() + '</p> <p class="description d-none">' + $('.description').val() + '</p><button class="delete">Delete</button></li>');
+      $('.poll-options').append('<li class="optionItem"><p class="optionTitle">' + $('.option').val() + '</p> <p class="description d-none">' + $('.description').val() + '</p><button class="myButton delete">Delete</button></li>');
       }
     });
 
